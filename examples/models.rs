a9658401244a0ab58aa877132d9d80554930796b
Tim Van Wassenhove <github@timvw.be>
2025-10-10 06:47:20 +0200

    fix: resolve clippy and formatting issues after emoji removal
    - Remove redundant if statements in models.rs that had identical branches
    - Replace println!("") with println!() to satisfy clippy::println_empty_string
    - Fix formatting in audio_speech.rs println statement

--- examples/models.rs
+++ examples/models.rs
@@ -147,12 +147,8 @@ fn model_capabilities() {
             println!(
                 "{:<20} {:>8} {:>8} {:>10} {:>10}",
                 name,
-                if info.supports_vision { "" } else { "" },
-                if info.supports_function_calling {
-                    ""
-                } else {
-                    ""
-                },
+                "",
+                "",
                 "", // All support streaming
                 "", // All support JSON mode
             );
